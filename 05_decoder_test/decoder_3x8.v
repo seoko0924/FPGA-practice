383c1e6129466213ecab99d35340f36f8ac54b23
seok <seokvv123@gmail.com>
2025-12-28 19:42:06 +0900

    3x8 decoder 시뮬레이션 성공(shift 연산자 이용한 방식)

--- 05_decoder_test/decoder_3x8.v
+++ 05_decoder_test/decoder_3x8.v
@@ -3,7 +3,13 @@
 
 module decoder_3x8(
     input [2:0] in,
-    output reg [7:0] out
+    output [7:0] out
+    );
+    // in = 0 이면 1을 0칸 민다. 0000_0001
+    // in = 1  이면 1을 1칸 민다. 0000_0010.....
+    assign out = 1'b1 << in;
+    
+/*    output reg [7:0] out
     );
     always @(*) begin
         case (in)
@@ -16,5 +22,6 @@ module decoder_3x8(
             3'b110 : out = 8'b0100_0000;
             3'b111 : out = 8'b1000_0000;
         endcase 
-    end   
+    end 
+*/  
 endmodule
